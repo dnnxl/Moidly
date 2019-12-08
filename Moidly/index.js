ae77a6e6f88a8bad9c20bff63af15cccde84bd7a
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-12-08 00:04:16 -0600

    Part Middleware functions

--- Moidly/index.js
+++ Moidly/index.js
@@ -1,68 +1,33 @@
 const Joi = require('joi');
+const helmet = require('helmet')
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
+const genres = require('./routes/genres');
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
+const config = require('config');
+
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+app.use(helmet());
 
-const genres = [
-  { id: 1, name: 'Action' },  
-  { id: 2, name: 'Horror' },  
-  { id: 3, name: 'Romance' },  
-  { id: 4, name: 'Thrillers' },
-  { id: 5, name: 'Comedy' },
-  { id: 6, name: 'Crime' },
-];
+app.use('/api/genres', genres);
 
-app.get('/api/genres', (req, res) => {
-  res.send(genres);
-});
+if(app.get('env') === 'development'){
+  app.use(morgan('tiny'));
+  startupDebugger('Enviroment ' + app.get('env'));
+  startupDebugger('Morgan enabled ...');
+  startupDebugger('Application Name: ' + config.get('name'));
 
-app.post('/api/genres', (req, res) => {
-  const { error } = validateGenre(req.body); 
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name
-  };
-  genres.push(genre);
-  res.send(genre);
-});
-
-app.put('/api/genres/:id', (req, res) => {
-  const genre = genres.find(c => c.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-
-  const { error } = validateGenre(req.body); 
-  if (error) return res.status(400).send(error.details[0].message);
-  
-  genre.name = req.body.name; 
-  res.send(genre);
-});
-
-app.delete('/api/genres/:id', (req, res) => {
-  const genre = genres.find(c => c.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-
-  res.send(genre);
-});
-
-app.get('/api/genres/:id', (req, res) => {
-  const genre = genres.find(c => c.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-  res.send(genre);
-});
-
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-
-  return Joi.validate(genre, schema);
 }
+
+// For database
+dbDebugger('Connected to the databse ...');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
